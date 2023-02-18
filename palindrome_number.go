1f95aace5559abee201764756b26d27def97c228
alff0x1f <alff0x1f@gmail.com>
2023-02-18 12:47:10 +0500

    palindrome number

---
+++ palindrome_number.go
@@ -0,0 +1,31 @@
+package leetcode
+
+func isPalindrome(x int) bool {
+	if x < 0 {
+		// negative numbers (like -5) can't be a palindrome because number can't end
+		// with minus sign
+		return false
+	}
+	digits := splitIntToDigits(x)
+	for i := 0; i < len(digits); i++ {
+		if digits[i] != digits[len(digits)-i-1] {
+			return false
+		}
+	}
+	return true
+}
+
+func splitIntToDigits(x int) []int {
+	// split number to single digits
+	reverseDigits := make([]int, 0)
+	for x > 0 {
+		reverseDigits = append(reverseDigits, x%10)
+		x = x / 10
+	}
+	countDigits := len(reverseDigits)
+	digits := make([]int, countDigits)
+	for idx := range reverseDigits {
+		digits[idx] = reverseDigits[countDigits-idx-1]
+	}
+	return reverseDigits
+}
---
+++ palindrome_number_test.go
@@ -0,0 +1,42 @@
+package leetcode
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestIsPalindrome(t *testing.T) {
+	//t.Skip()
+	var tests = []struct {
+		input  int
+		output bool
+	}{
+		{121, true},
+		{-121, false},
+		{10, false},
+	}
+	for _, tst := range tests {
+		answer := isPalindrome(tst.input)
+		if answer != tst.output {
+			t.Errorf("Wrong decision (%t) for %d", answer, tst.input)
+		}
+	}
+}
+
+func TestSplitIntToDigits(t *testing.T) {
+	var tests = []struct {
+		input  int
+		output []int
+	}{
+		{1, []int{1}},
+		{100, []int{1, 0, 0}},
+		{12345, []int{1, 2, 3, 4, 5}},
+	}
+	for _, tst := range tests {
+		digits := splitIntToDigits(tst.input)
+		if len(digits) != len(tst.output) {
+			digitsStr := fmt.Sprint(digits)
+			t.Errorf("error in %d (%s)", tst.input, digitsStr)
+		}
+	}
+}
