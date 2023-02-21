6945f829fc48de691f30810bd46baedf6f6c50c2
alff0x1f <alff0x1f@gmail.com>
2023-02-21 10:21:45 +0500

    540. Single Element in a Sorted Array

--- Single Element in a Sorted Array/single_element_in_a_sorted_array.go
+++ Single Element in a Sorted Array/single_element_in_a_sorted_array.go
@@ -1,6 +1,12 @@
 package Single_Element_in_a_Sorted_Array
 
 func singleNonDuplicate(nums []int) int {
+	// You are given a sorted array consisting of only integers where every element
+	// appears exactly twice, except for one element which appears exactly once.
+	//
+	// Return the single element that appears only once.
+	//
+	// Your solution must run in O(log n) time and O(1) space.
 	start := 0
 	end := len(nums) - 1
 	for end-start > 0 {
